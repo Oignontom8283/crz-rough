b3eeb0e2a8646a10b2ae4bc62ee6e1f42dcbfcb6
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-04-26 21:17:13 +0200

    feat: Implement command-line argument parsing with clap

---
+++ src/cli.rs
@@ -0,0 +1,16 @@
+
+use clap::Parser;
+use crate::common::{APP_NAME};
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about = APP_NAME)]
+pub struct Args {
+    /// Character used to separate fields in the value
+    #[arg(short, long, default_value = ",")]
+    pub delim: String,
+
+    /// Default position of the cursor 
+    #[arg(short, long, default_value_t = 0)]
+    pub index: u64,
+}
